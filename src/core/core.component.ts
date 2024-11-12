47dccae82a112ce57d22b9cb1ed5425e66e79245
Tim 'Piepmatz' Hesse <git@cptpiepmatz.de>
2024-11-12 12:29:48 +0100

    updated navbar

--- src/core/core.component.ts
+++ src/core/core.component.ts
@@ -1,13 +1,28 @@
-import {Component} from "@angular/core";
+import {signal, Component} from "@angular/core";
 import {provideIcons, NgIconComponent} from "@ng-icons/core";
-import {remixLogoutBoxLine, remixMenu2Line} from "@ng-icons/remixicon";
+import {
+  remixLogoutBoxLine,
+  remixMenu2Line,
+  remixTranslate2,
+} from "@ng-icons/remixicon";
+
+import {IsActiveToggleDirective} from "../common/directives/is-active-toggle.directive";
+import {signals} from "../common/signals";
 
 @Component({
   selector: "wisdom-core",
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, IsActiveToggleDirective],
   templateUrl: "./core.component.html",
   styleUrl: "./core.component.scss",
-  providers: [provideIcons({remixMenu2Line, remixLogoutBoxLine})],
+  providers: [
+    provideIcons({
+      remixMenu2Line,
+      remixLogoutBoxLine,
+      remixTranslate2,
+    }),
+  ],
 })
-export class CoreComponent {}
+export class CoreComponent {
+  userDropdownActive = signals.toggleable(false);
+}
