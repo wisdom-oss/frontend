6d43eb9f5b9aaf35250606a6b5728f9e9b113dc2
Tim 'Piepmatz' Hesse <git@cptpiepmatz.de>
2024-11-21 16:18:20 +0100

    generalized the signals.toggleable

--- src/common/signals.ts
+++ src/common/signals.ts
@@ -11,11 +11,17 @@ import {signal, WritableSignal} from "@angular/core";
 export namespace signals {
   export type ToggleableSignal<S extends WritableSignal<boolean>> = S & {
     toggle(): void;
-  }
-  
-  export function toggleable(initial: boolean): ToggleableSignal<WritableSignal<boolean>>;
-  export function toggleable<S extends WritableSignal<boolean>>(initial: S): ToggleableSignal<S>; 
-  export function toggleable<S extends WritableSignal<boolean>>(initial: boolean | S) {
+  };
+
+  export function toggleable(
+    initial: boolean,
+  ): ToggleableSignal<WritableSignal<boolean>>;
+  export function toggleable<S extends WritableSignal<boolean>>(
+    initial: S,
+  ): ToggleableSignal<S>;
+  export function toggleable<S extends WritableSignal<boolean>>(
+    initial: boolean | S,
+  ) {
     let s = typeof initial == "boolean" ? signal(initial) : initial;
     return Object.defineProperty(s, "toggle", {
       writable: false,
