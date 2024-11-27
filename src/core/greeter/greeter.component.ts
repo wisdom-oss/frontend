1015f062d05bdd1531a95f075152b3c90c325bd7
Tim Hesse <tim.hesse@uni-oldenburg.de>
2024-11-27 14:58:46 +0100

    added basic greeter

---
+++ src/core/greeter/greeter.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+import {provideIcons, NgIcon} from "@ng-icons/core";
+import {remixExternalLinkFill, remixGithubFill} from "@ng-icons/remixicon";
+
+@Component({
+  selector: "wisdom-greeter",
+  standalone: true,
+  imports: [NgIcon],
+  templateUrl: "./greeter.component.html",
+  styles: ``,
+  providers: [
+    provideIcons({
+      remixGithubFill,
+      remixExternalLinkFill,
+    }),
+  ],
+})
+export class GreeterComponent {}
