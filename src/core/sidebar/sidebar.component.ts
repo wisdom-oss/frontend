f2d32fa60d678ffce40909ece1b898dde12efca3
Tim Hesse <tim.hesse@uni-oldenburg.de>
2024-12-11 16:04:05 +0100

    updated sidebar

--- src/core/sidebar/sidebar.component.ts
+++ src/core/sidebar/sidebar.component.ts
@@ -1,9 +1,18 @@
 import {Component} from "@angular/core";
+import {RouterLink} from "@angular/router";
+import {provideIcons, NgIconComponent} from "@ng-icons/core";
+import {remixBookLine, remixBookShelfLine} from "@ng-icons/remixicon";
 
 @Component({
   selector: "sidebar",
-  imports: [],
+  imports: [NgIconComponent, RouterLink],
   templateUrl: "./sidebar.component.html",
   styleUrl: "./sidebar.component.scss",
+  providers: [
+    provideIcons({
+      remixBookShelfLine,
+      remixBookLine,
+    }),
+  ],
 })
 export class SidebarComponent {}
