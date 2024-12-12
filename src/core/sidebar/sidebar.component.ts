910da2a544699d432fa971d92f32d527e10d930a
Tim Hesse <tim.hesse@uni-oldenburg.de>
2024-12-12 19:40:14 +0100

    let sidebar highlight current route

--- src/core/sidebar/sidebar.component.ts
+++ src/core/sidebar/sidebar.component.ts
@@ -1,11 +1,14 @@
-import {Component} from "@angular/core";
-import {RouterLink} from "@angular/router";
+import {ViewChildren, Component, AfterViewInit, QueryList} from "@angular/core";
+import {NavigationEnd, RouterLink, Router} from "@angular/router";
 import {provideIcons, NgIconComponent} from "@ng-icons/core";
 import {remixDatabase2Fill, remixMap2Fill} from "@ng-icons/remixicon";
+import {filter} from "rxjs";
+
+import {SidebarLinkDirective} from "./sidebar-link.directive";
 
 @Component({
   selector: "sidebar",
-  imports: [NgIconComponent, RouterLink],
+  imports: [NgIconComponent, RouterLink, SidebarLinkDirective],
   templateUrl: "./sidebar.component.html",
   styleUrl: "./sidebar.component.scss",
   providers: [
@@ -15,4 +18,30 @@ import {remixDatabase2Fill, remixMap2Fill} from "@ng-icons/remixicon";
     }),
   ],
 })
-export class SidebarComponent {}
+export class SidebarComponent implements AfterViewInit {
+  @ViewChildren(SidebarLinkDirective)
+  routerLinks?: QueryList<SidebarLinkDirective>;
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    this.highlightCurrentRoute();
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.highlightCurrentRoute());
+  }
+
+  private highlightCurrentRoute() {
+    this.routerLinks?.forEach(routerLink => {
+      let link = routerLink.routerLink();
+      routerLink.isActive.set(
+        this.router.isActive(link, {
+          matrixParams: "ignored",
+          queryParams: "ignored",
+          paths: "exact",
+          fragment: "ignored",
+        }),
+      );
+    });
+  }
+}
