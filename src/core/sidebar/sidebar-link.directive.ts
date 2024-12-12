135242845f579652f5613b8d35f0356c51d08a57
Tim Hesse <tim.hesse@uni-oldenburg.de>
2024-12-12 19:40:14 +0100

    let sidebar highlight current route

---
+++ src/core/sidebar/sidebar-link.directive.ts
@@ -0,0 +1,10 @@
+import {input, signal, Directive} from "@angular/core";
+
+@Directive({
+  selector: "a[routerLink]",
+  host: {"[class.is-active]": "isActive()"},
+})
+export class SidebarLinkDirective {
+  readonly routerLink = input.required<string>();
+  readonly isActive = signal(true);
+}
