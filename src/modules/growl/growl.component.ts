a814045f381bc185836800439d136b7eb1137d7f
Tim 'Piepmatz' Hesse <git@cptpiepmatz.de>
2024-12-17 06:47:16 +0100

    started to add growl marker

--- src/modules/growl/growl.component.ts
+++ src/modules/growl/growl.component.ts
@@ -1,20 +1,54 @@
-import {Component, OnInit} from "@angular/core";
-import {LayerComponent, MapComponent} from "@maplibre/ngx-maplibre-gl";
+import {AsyncPipe} from "@angular/common";
+import {
+  AfterViewChecked,
+  ViewChild,
+  Component,
+  OnInit,
+  AfterViewInit,
+  HostListener,
+} from "@angular/core";
+import {
+  LayerComponent,
+  MapComponent,
+  MarkerComponent,
+} from "@maplibre/ngx-maplibre-gl";
+import {Point} from "geojson";
 import {StyleSpecification} from "maplibre-gl";
 
+import {GroundwaterLevelStationMarkerComponent} from "./map/groundwater-level-station-marker/groundwater-level-station-marker.component";
 import {GeoDataService} from "../../api/geo-data.service";
 import colorful from "../../common/map/styles/colorful.json";
+import {typeUtils} from "../../common/type-utils";
+
+type Points = typeUtils.UpdateElements<
+  GeoDataService.LayerContents,
+  "geometry",
+  {geometry: Point}
+>;
 
 @Component({
-  selector: "wisdom-growl",
-  imports: [MapComponent, LayerComponent],
+  selector: "growl",
+  imports: [
+    MapComponent,
+    MarkerComponent,
+    GroundwaterLevelStationMarkerComponent,
+    AsyncPipe,
+  ],
   templateUrl: "./growl.component.html",
   styles: ``,
 })
 export class GrowlComponent implements OnInit {
   readonly style = colorful as any as StyleSpecification;
+  @ViewChild(MapComponent) map?: MapComponent;
 
-  constructor(private geo: GeoDataService) {}
+  readonly groundwaterMeasurementStations: Promise<Points>;
+
+  constructor(private geo: GeoDataService) {
+    this.groundwaterMeasurementStations = this.geo
+      .fetchLayerContents("groundwater_measurement_stations")
+      .then(contents => contents ?? [])
+      .then(p => p.filter(({geometry}) => geometry.type === "Point") as Points);
+  }
 
   ngOnInit(): void {
     this.geo
@@ -23,5 +57,11 @@ export class GrowlComponent implements OnInit {
     this.geo
       .fetchLayerContents("groundwater_measurement_stations")
       .then(data => console.log(data));
+  }
+
+  @HostListener("window:load")
+  onLoad() {
+    // enforce resizing on initial load
+    this.map!.mapInstance.resize();
   }
 }
---
+++ src/modules/growl/map/groundwater-level-station-marker/groundwater-level-station-marker.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "growl-groundwater-level-station-marker",
+  imports: [],
+  templateUrl: "./groundwater-level-station-marker.component.svg",
+  styles: `
+    svg {
+      height: 2em;
+      width: 2em;
+    }
+  `,
+})
+export class GroundwaterLevelStationMarkerComponent {}
