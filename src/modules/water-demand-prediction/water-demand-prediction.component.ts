3c9b866e99101695a516a132e96860b37db77a54
Jan Stüven <stueven.jan@web.de>
2025-01-03 12:06:43 +0100

    added water-demand-prediction component

---
+++ src/modules/water-demand-prediction/water-demand-prediction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wisdom-water-demand-prediction',
+  imports: [],
+  templateUrl: './water-demand-prediction.component.html',
+  styles: ``
+})
+export class WaterDemandPredictionComponent {
+
+}
