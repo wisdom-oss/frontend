ae4278c1fdb8606b3661b7e95b6ea996ed256f34
Jan Stüven <stueven.jan@web.de>
2025-01-03 12:22:24 +0100

    fixed route component

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,14 @@ import {Routes} from "@angular/router";
 import {CallbackComponent} from "./core/auth/callback/callback.component";
 import {CoreComponent} from "./core/core.component";
 import {GreeterComponent} from "./core/greeter/greeter.component";
+import { WaterDemandPredictionComponent } from "./modules/water-demand-prediction/water-demand-prediction.component";
 
 export const routes: Routes = [
   { path: "callback", component: CallbackComponent },
   { path: "", component: CoreComponent, children: [
       { path: "", component: GreeterComponent }, // Default child route
+      { path: "water-demand-prediction", component: WaterDemandPredictionComponent }, // Default child route
+
     ] 
   },
   { path: "**", redirectTo: "" }, // Wildcard fallback
