a0999eca2d75189e8db42401eae3c8e81f2d87b0
Jan Stüven <stueven.jan@web.de>
2025-01-03 11:54:04 +0100

    fixed routes.ts to display routes correct
    when using "**" wildcard as a route before defining children, no route can be activated. So the basic component at route "" will always be displayed.
    
    Setting the wild card at the end of the segment workarounds the problem. (Verified and tested on water prediction branch)

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,13 @@ import {Routes} from "@angular/router";
 import {CallbackComponent} from "./core/auth/callback/callback.component";
 import {CoreComponent} from "./core/core.component";
 import {GreeterComponent} from "./core/greeter/greeter.component";
+import { WaterDemandPredictionComponent } from "./modules/water-demand-prediction/water-demand-prediction.component";
 
 export const routes: Routes = [
-  {path: "callback", component: CallbackComponent},
-  {
-    path: "**",
-    component: CoreComponent,
-    children: [{path: "", component: GreeterComponent}],
+  { path: "callback", component: CallbackComponent },
+  { path: "", component: CoreComponent, children: [
+      { path: "", component: GreeterComponent }, // Default child route
+    ] 
   },
+  { path: "**", redirectTo: "" }, // Wildcard fallback
 ];
