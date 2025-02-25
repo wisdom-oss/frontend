c19522d13d7d79350c94659504cfdebe4753ff4b
Tim 'Piepmatz' Hesse <git@cptpiepmatz.de>
2025-02-25 19:42:00 +0100

    added a permissions guard

---
+++ src/core/auth/permissions.guard.ts
@@ -0,0 +1,47 @@
+import {inject} from "@angular/core";
+import {CanActivateFn} from "@angular/router";
+
+import {UserService} from "../user.service";
+
+/**
+ * A route guard that checks if a user has the required permissions to access a
+ * route.
+ *
+ * This function accepts a list of required permissions (scopes) and returns a
+ * {@link CanActivateFn}.
+ * All specified scopes must be satisfied for access to be granted.
+ * - The `*` level allows any permission level.
+ * - The `*` service allows any service.
+ * - The `*:*` permission grants access to all permissions.
+ *
+ * @param scopes A list of permission scopes in the format `<service>:<level>`,
+ *               where `level` can be `read`, `write`, `delete`, or `*`.
+ */
+export function permissionsGuard(
+  ...scopes: permissionsGuard.Scope[]
+): CanActivateFn {
+  return (_route, _state) => {
+    let userService = inject(UserService);
+    let userPermissions = userService.userDetails()?.permissions ?? {};
+
+    for (let scope of scopes) {
+      let split = scope.split(":");
+      let level = split.pop()!; // scope type enforces that a level must exist
+      let service = split.join(":");
+
+      let userLevels = userPermissions[service] ?? userPermissions["*"] ?? [];
+      if (userLevels.includes("*")) continue;
+      if (!userLevels.includes(level)) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+}
+
+export namespace permissionsGuard {
+  export type Service = string;
+  export type Level = "read" | "write" | "delete" | "*";
+  export type Scope = `${Service}:${Level}`;
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import {Routes} from "@angular/router";
 
 import {CallbackComponent} from "./core/auth/callback/callback.component";
 import {CoreComponent} from "./core/core.component";
+import {permissionsGuard} from "./core/auth/permissions.guard";
 import {GreeterComponent} from "./core/greeter/greeter.component";
 import {GrowlComponent} from "./modules/growl/growl.component";
 
@@ -13,6 +14,11 @@ export const routes: Routes = [
     children: [
       {path: "", component: GreeterComponent}, // Default child route
       {path: "growl", component: GrowlComponent},
+      {
+        path: "oowv/action-map",
+        component: GreeterComponent,
+        canActivate: [permissionsGuard("geodata:read")],
+      },
     ],
   },
   {path: "**", redirectTo: ""}, // Wildcard fallback
