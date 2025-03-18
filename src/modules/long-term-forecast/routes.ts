d52f0f216dc513f61646b4d77239ff1e4e2c34fa
Tim 'Piepmatz' Hesse <git@cptpiepmatz.de>
2025-03-18 15:13:53 +0100

    add query parameter guard

--- src/modules/long-term-forecast/routes.ts
+++ src/modules/long-term-forecast/routes.ts
@@ -2,9 +2,15 @@ import {Routes} from "@angular/router";
 
 import {MapSelectViewComponent} from "./views/map-select-view/map-select-view.component";
 import {ResultDataViewComponent} from "./views/result-data-view/result-data-view.component";
+import {queryParameterGuard} from "../../core/query-parameter.guard";
 
 export const longTermForecastRoutes: Routes = [
   {path: "", component: MapSelectViewComponent},
-  {path: "results", component: ResultDataViewComponent},
+  {
+    path: "results",
+    component: ResultDataViewComponent,
+    // TODO: figure out, how I can just write "" here
+    canActivate: [queryParameterGuard("key", "/long-term-forecast")],
+  },
   {path: "**", redirectTo: ""}, // wildcard fallback
 ];
