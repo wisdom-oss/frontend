2ac6d734ae6d8c54189fd3d375688f3ba3d4d252
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-03-25 11:05:39 +0100

    pulled some common updates from feat/prognosis

--- src/api/geo-data.service.ts
+++ src/api/geo-data.service.ts
@@ -11,7 +11,6 @@ import {GeoJSON} from "geojson";
 import {firstValueFrom} from "rxjs";
 
 import {httpContexts} from "../common/http-contexts";
-import {typeUtils} from "../common/type-utils";
 
 const URL = "/api/geodata" as const;
 
@@ -23,7 +22,7 @@ export class GeoDataService {
 
   fetchAvailableLayers(): Promise<GeoDataService.AvailableLayers> {
     return firstValueFrom(
-      this.http.get<GeoDataService.AvailableLayers>(`${URL}/`, {
+      this.http.get<GeoDataService.AvailableLayers>(`${URL}/v2/`, {
         context: new HttpContext().set(
           httpContexts.validateSchema,
           AVAILABLE_LAYERS_SCHEMA,
@@ -37,12 +36,15 @@ export class GeoDataService {
   ): Promise<GeoDataService.LayerInformation | null> {
     try {
       return await firstValueFrom(
-        this.http.get<GeoDataService.LayerInformation>(`${URL}/${layerRef}`, {
-          context: new HttpContext().set(
-            httpContexts.validateSchema,
-            LAYER_INFORMATION,
-          ),
-        }),
+        this.http.get<GeoDataService.LayerInformation>(
+          `${URL}/v2/${layerRef}`,
+          {
+            context: new HttpContext().set(
+              httpContexts.validateSchema,
+              LAYER_INFORMATION,
+            ),
+          },
+        ),
       );
     } catch (error) {
       if (!(error instanceof HttpErrorResponse)) throw error;
@@ -61,7 +63,7 @@ export class GeoDataService {
     cacheTtl = dayjs.duration(1, "week"),
   ): Promise<GeoDataService.LayerContents | null> {
     try {
-      let url = `${URL}/content/${layerRef}`;
+      let url = `${URL}/v2/content/${layerRef}`;
       if (filter) {
         let queryParams = [];
         for (let [key, value] of Object.entries({
@@ -87,9 +89,10 @@ export class GeoDataService {
     }
   }
 
-  identify(keys: string[]): Promise<GeoDataService.IdentifiedObjects> {
-    let queryParams = keys.map(k => `key=${k}`);
-    let url = `${URL}/identify?${queryParams.join("=")}`;
+  identify(keys: Iterable<string>): Promise<GeoDataService.IdentifiedObjects> {
+    let queryParams: string[] = [];
+    for (let key of keys) queryParams.push(`key=${key}`);
+    let url = `${URL}/v1/identify?${queryParams.join("&")}`;
     return firstValueFrom(
       this.http.get<GeoDataService.IdentifiedObjects>(url, {
         context: new HttpContext().set(
@@ -104,11 +107,14 @@ export class GeoDataService {
 export namespace GeoDataService {
   export type LayerInformation = JTDDataType<typeof LAYER_INFORMATION>;
   export type AvailableLayers = JTDDataType<typeof AVAILABLE_LAYERS_SCHEMA>;
-  export type LayerContents = typeUtils.UpdateElements<
+  export type LayerContent = Omit<
+    JTDDataType<typeof LAYER_CONTENT>,
+    "geometry"
+  > & {geometry: GeoJSON};
+  export type LayerContents = Omit<
     JTDDataType<typeof LAYER_CONTENTS>,
-    "geometry",
-    {geometry: GeoJSON}
-  >;
+    "data"
+  > & {data: LayerContent[]};
   export type IdentifiedObjects = JTDDataType<typeof IDENTIFIED_OBJECTS>;
 }
 
@@ -121,6 +127,7 @@ const LAYER_INFORMATION = {
   optionalProperties: {
     description: {type: "string"},
     attribution: {type: "string"},
+    attributionURL: {type: "string"},
     crs: {type: "uint32"},
     private: {type: "boolean"},
   },
@@ -149,7 +156,13 @@ const LAYER_CONTENT = {
 } as const;
 
 const LAYER_CONTENTS = {
-  elements: LAYER_CONTENT,
+  properties: {
+    data: {elements: LAYER_CONTENT},
+  },
+  optionalProperties: {
+    attribution: {type: "string", nullable: true},
+    attributionURL: {type: "string", nullable: true},
+  },
 } as const;
 
 const IDENTIFIED_OBJECTS = {
---
+++ src/common/utils/defaults.ts
@@ -0,0 +1,41 @@
+export function defaults<K extends string | number | symbol, V>(
+  initial: Record<K, Partial<V>>,
+): Record<K, Partial<V>>;
+export function defaults<K extends string | number | symbol, V>(
+  initial: Record<K, V>,
+  defaultGenerator: (key: K) => V,
+): Record<K, V>;
+
+/**
+ * Wraps a record so that any missing key will automatically get a default value.
+ *
+ * If a `defaultGenerator` is provided, it's called with the missing key to
+ * create the value.
+ * If no generator is given, missing keys will default to an empty object (`{}`).
+ * This avoids having to check for key presence before using the value.
+ *
+ * @param initial The base record to extend. This object will be modified directly.
+ * @param defaultGenerator Optional function that receives the missing key and returns a default value.
+ *
+ * @example
+ * ```ts
+ * const map = defaults({} as Record<string, string[]>, (key) => [key]);
+ * map["a"].push("hello"); // "a" is set to ["a"] automatically
+ * ```
+ *
+ * @remarks
+ * The `initial` object is not cloned.
+ * Any changes to the returned record will directly affect the original one.
+ */
+export function defaults<K extends string | number | symbol, V>(
+  initial: Record<K, V>,
+  defaultGenerator?: (key: K) => V,
+): Record<K, V> {
+  let gen = defaultGenerator ?? (() => ({}));
+  return new Proxy(initial, {
+    get(_target, prop, _receiver) {
+      if (!(prop in initial)) initial[prop as K] = gen(prop as K) as V;
+      return initial[prop as K];
+    },
+  });
+}
