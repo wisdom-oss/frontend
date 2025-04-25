7831f9d60e2b75d6aceb557c2ad63025c1822944
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-04-26 00:01:07 +0200

    try to get water right details in detail view

--- src/modules/water-rights/views/detail-view/detail-view.component.ts
+++ src/modules/water-rights/views/detail-view/detail-view.component.ts
@@ -1,18 +1,18 @@
-import {Component, computed, effect, Signal} from "@angular/core";
-import { toSignal } from "@angular/core/rxjs-interop";
+import {Component, effect, Signal} from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { WaterRightsService } from "../../../../api/water-rights.service";
+import { signals } from "../../../../common/signals";
 
 @Component({
   imports: [],
   templateUrl: "./detail-view.component.html",
 })
 export class DetailViewComponent {
-  protected no: Signal<number>;
+  protected waterRight: Signal<undefined | WaterRightsService.WaterRightDetails>;
   
-  constructor(route: ActivatedRoute) {
-    let queryParams = toSignal(route.queryParamMap);
-    this.no = computed(() => +queryParams()!.get("no")!);
-
-    effect(() => console.log(this.no()));
+  constructor(route: ActivatedRoute, service: WaterRightsService) {
+    this.waterRight = signals.fromPromise(service.fetchWaterRightDetails(+route.snapshot.queryParams["no"]!));
+  
+    effect(() => console.log(this.waterRight()));
   }
 }
