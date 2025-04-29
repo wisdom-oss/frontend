6c6ef733f1ca3664099a2ae761f6496a2076d173
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-04-29 13:38:56 +0200

    remove obsolete entry point component

--- src/modules/long-term-forecast/long-term-forecast.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-import {RouterOutlet} from "@angular/router";
-
-@Component({
-  imports: [RouterOutlet],
-  template: "<router-outlet></router-outlet>",
-  styles: ``,
-})
-export class LongTermForecastComponent {}
