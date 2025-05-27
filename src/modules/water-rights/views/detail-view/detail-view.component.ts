8bd6ca9c771f25e7c9ba304fa81d6d151ca91f40
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-05-27 21:20:33 +0200

    some more small fixes

--- src/modules/water-rights/views/detail-view/detail-view.component.ts
+++ src/modules/water-rights/views/detail-view/detail-view.component.ts
@@ -174,7 +174,7 @@ export class DetailViewComponent {
     geo: GeoDataService,
     dataSignal: DetailViewComponent["data"],
   ): Signal<undefined | UsageLocations> {
-    let allUsageLocations = signals.fromPromise(
+    let allUsageLocations = signals.map(
       geo.fetchLayerContents(
         "water_right_usage_locations",
         undefined,
