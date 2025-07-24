0f1ac928986811855deddee9397fe190cb3d6627
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-07-24 11:10:51 +0200

    fmt

--- src/core/greeter/greeter.component.ts
+++ src/core/greeter/greeter.component.ts
@@ -1,7 +1,6 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
 import {provideIcons, NgIcon} from "@ng-icons/core";
 import {remixExternalLinkFill, remixGithubFill} from "@ng-icons/remixicon";
-
 
 @Component({
   selector: "wisdom-greeter",
