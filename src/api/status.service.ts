fd3317d1cf576e594ef185f86842f630fc39a4e0
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-08-07 12:28:21 +0200

    update status service to proper types

--- src/api/status.service.ts
+++ src/api/status.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from "@angular/core";
-import typia from "typia";
-
+import typia, { tags } from "typia";
+import { typeUtils } from "../common/utils/type-utils";
+import dayjs, { Dayjs } from "dayjs";
+import { Duration } from "dayjs/plugin/duration";
 import {api} from "../common/api";
 
 const URL = "/api/status" as const;
@@ -9,16 +11,57 @@ const URL = "/api/status" as const;
   providedIn: "root",
 })
 export class StatusService {
-  private statusSocket?: api.Socket<any, any, any>;
+  private statusSocket?: api.Socket<Self.Status, Self.Subscribe>;
 
   get socket() {
     if (this.statusSocket) return this.statusSocket;
 
+    let parse = (input: RawStatus): Self.Status => input.map(element => ({
+        ...element,
+        lastUpdate: dayjs(element.lastUpdate)
+      }));
+
+    let serialize = (input: Self.Subscribe): SerializedSubscribe => ({
+        ...input,
+        data: {
+          ...input.data,
+          paths: input.data.paths,
+          updateInterval: input.data.updateInterval.toISOString(),
+        }
+    });
+
     this.statusSocket = api.socket({
-      url: `${URL}/v1/testing`,
-      validate: typia.createValidate<any>(),
+      url: `${URL}/v1/`,
+      validateRaw: typia.createValidate<RawStatus>(),
+      validate: typia.createValidate<Self.Status>(),
+      parse,
+      serialize,
     });
 
     return this.statusSocket;
   }
 }
+
+type RawStatus = Array<{
+  path: string;
+  lastUpdate: string & tags.Format<"date-time">,
+  status: "ok" | "down" | "limited",
+}>;
+
+type SerializedSubscribe = {
+    command: "subscribe",
+    id: string | number,
+    data: {
+      paths: string[],
+      updateInterval: string & tags.Format<"duration">,
+    },
+  };
+
+export namespace StatusService {
+  export type Status = typeUtils.UpdateElements<RawStatus, "lastUpdate", {lastUpdate: Dayjs}>;
+
+  export type Subscribe = typeUtils.Overwrite<SerializedSubscribe, {data: typeUtils.Overwrite<SerializedSubscribe["data"], {updateInterval: Duration}>}>;
+};
+
+import Self = StatusService;
+
