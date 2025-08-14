8f79ca59cfaee2194708f148c1966c79eef84601
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-08-14 15:26:19 +0200

    import global.ts first

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import {registerLocaleData, DOCUMENT} from "@angular/common";
+import "./global";
+
+import {registerLocaleData} from "@angular/common";
 import localeDe from "@angular/common/locales/de";
 import localeDeExtra from "@angular/common/locales/extra/de";
-import {inject, Component} from "@angular/core";
+import {Component} from "@angular/core";
 import {bootstrapApplication} from "@angular/platform-browser";
 import {RouterOutlet} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
@@ -15,16 +17,12 @@ import durationExt from "./core/dayjs/duration-ext.plugin";
 
 import "dayjs/locale/de";
 
-import "./global";
-
 @Component({
   selector: "app",
   imports: [RouterOutlet],
   template: "<router-outlet></router-outlet>",
 })
 export class AppComponent {
-  private document = inject(DOCUMENT);
-
   constructor(translate: TranslateService) {
     dayjs.extend(duration);
     dayjs.extend(relativeTime);
