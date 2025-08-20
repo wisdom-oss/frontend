4199f81da57369477da5052e3719378d50d682ec
Pascal Säfken <pascal.saefken@uni-oldenburg.de>
2025-08-20 16:40:19 +0200

    Started with designing the overview component

--- src/modules/braintank/tabs/overview/overview.component.ts
+++ src/modules/braintank/tabs/overview/overview.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import { remixContrastDrop2Line, remixTimeLine, remixHome8Line, remixMapPin2Line, remixWaterPercentLine } from '@ng-icons/remixicon';
+import { TranslateDirective } from '@ngx-translate/core';
 import { ModelViewComponent } from "../../model-view/model-view.component";
 
 
 @Component({
   selector: 'braintank-overview',
-  imports: [ModelViewComponent],
-  templateUrl: './overview.component.html'
+  imports: [
+    ModelViewComponent,
+    TranslateDirective,
+    NgIconComponent,
+],
+  templateUrl: './overview.component.html',
+  providers: [
+    provideIcons({
+      remixContrastDrop2Line,
+      remixWaterPercentLine,
+      remixTimeLine,
+      remixHome8Line,
+      remixMapPin2Line,
+    }),
+  ],
 })
 export class OverviewComponent {
-  
+  waterLevel : WritableSignal<number> = signal(30);
 }
