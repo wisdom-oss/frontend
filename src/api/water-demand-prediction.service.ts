40d196aa2375700398d98d97922d1396128b3912
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-08-21 15:06:36 +0200

    better describe response types

--- src/api/water-demand-prediction.service.ts
+++ src/api/water-demand-prediction.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import typia from "typia";
+import typia, {tags} from "typia";
 
 import {api} from "../common/api";
 
@@ -87,30 +87,32 @@ export class WaterDemandPredictionService {
 }
 
 export namespace WaterDemandPredictionService {
+  export type Resolution = "hourly" | "daily" | "weekly";
+  export type Timeframe = "one day" | "one week" | "one month" | "three months" | "six months" | "one year" | "all";
   export type WeatherColumns = Record<string, string>;
   export type MeterNames = Record<string, string>;
   export type SingleSmartmeter = {
     name: string;
-    resolution: string;
-    timeframe: string;
-    value: number[];
-    date: string[];
+    resolution: Resolution;
+    timeframe: Timeframe;
+    value: (number & tags.Type<"double">)[];
+    date: (string & tags.Format<"date-time">)[];
   };
   export type PredictedSmartmeter = {
-    aic: number;
-    date: string[];
-    fitTime: number;
-    lowerConfValues: number[];
-    meanAbsoluteError: number;
-    meanSquaredError: number;
+    aic: number & tags.Type<"double">;
+    date: (string & tags.Format<"date-time">)[];
+    fitTime: number & tags.Type<"double">;
+    lowerConfValues: (number & tags.Type<"double">)[];
+    meanAbsoluteError: number & tags.Type<"double">;
+    meanSquaredError: number & tags.Type<"double">;
     name: string;
-    r2: number;
-    realValue: number[];
-    resolution: string;
-    rootOfmeanSquaredError: number;
-    timeframe: string;
-    upperConfValues: number[];
-    value: number[];
+    r2: number & tags.Type<"double">;
+    realValue: (number & tags.Type<"double">)[];
+    resolution: Resolution;
+    rootOfmeanSquaredError: number & tags.Type<"double">;
+    timeframe: Timeframe;
+    upperConfValues: (number & tags.Type<"double">)[];
+    value: (number & tags.Type<"double">)[];
   };
 }
 
