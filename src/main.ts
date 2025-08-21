e0cb7b45fb5a10cf765ad87cc4794162194067bd
Piepmatz <tim.hesse@uni-oldenburg.de>
2025-08-21 13:49:27 +0200

    Update @ngx-translate to 17 (#162)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import {registerLocaleData, DOCUMENT} from "@angular/common";
+import {registerLocaleData} from "@angular/common";
 import localeDe from "@angular/common/locales/de";
 import localeDeExtra from "@angular/common/locales/extra/de";
-import {inject, Component} from "@angular/core";
+import {Component} from "@angular/core";
 import {bootstrapApplication} from "@angular/platform-browser";
 import {RouterOutlet} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
@@ -13,19 +13,20 @@ import {wisdomAppConfig} from "./config";
 import {configureTranslations} from "./i18n";
 import durationExt from "./core/dayjs/duration-ext.plugin";
 
+import "dayjs/locale/de";
+
 @Component({
   selector: "app",
   imports: [RouterOutlet],
   template: "<router-outlet></router-outlet>",
 })
 export class AppComponent {
-  private document = inject(DOCUMENT);
-
   constructor(translate: TranslateService) {
     dayjs.extend(duration);
     dayjs.extend(relativeTime);
     dayjs.extend(durationExt);
     configureTranslations(translate);
+    translate.onLangChange.subscribe(({lang}) => dayjs.locale(lang));
     registerLocaleData(localeDe, "de", localeDeExtra);
   }
 }
