b256da2fd9b8cf4c7058336c807aecaa0b5b0c8f
Pascal Säfken <pascal.saefken@uni-oldenburg.de>
2025-08-22 15:04:23 +0200

    Added some signal variables to overview

--- src/modules/braintank/tabs/overview/overview.component.ts
+++ src/modules/braintank/tabs/overview/overview.component.ts
@@ -3,6 +3,7 @@ import { NgIconComponent, provideIcons } from "@ng-icons/core";
 import { remixContrastDrop2Line, remixTimeLine, remixHome8Line, remixMapPin2Line, remixWaterPercentLine } from '@ng-icons/remixicon';
 import { TranslateDirective } from '@ngx-translate/core';
 import { ModelViewComponent } from "../../model-view/model-view.component";
+import dayjs, { Dayjs } from 'dayjs';
 
 
 @Component({
@@ -25,4 +26,10 @@ import { ModelViewComponent } from "../../model-view/model-view.component";
 })
 export class OverviewComponent {
   waterLevel : WritableSignal<number> = signal(30);
+  time : WritableSignal<Dayjs> = signal(dayjs());
+  roofSize : WritableSignal<number> = signal(160);
+  lat : WritableSignal<number> = signal(53.146);
+  long : WritableSignal<number> = signal(8.185);
+  draining : WritableSignal<boolean> = signal(true);
+  drainingTime: WritableSignal<Dayjs> = signal(dayjs().subtract(30, 'minutes'));
 }
