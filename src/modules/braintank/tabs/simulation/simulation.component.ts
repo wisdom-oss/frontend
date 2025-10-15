5a26e181f6f3abb526d369a2b69dfdf306350f1c
Pascal Säfken <pascal.saefken@uni-oldenburg.de>
2025-10-15 16:11:49 +0200

    Added two modals and their functionality

--- src/modules/braintank/tabs/simulation/simulation.component.ts
+++ src/modules/braintank/tabs/simulation/simulation.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, effect, signal, viewChild, WritableSignal } from '@angular/core';
 import { ModelViewComponent } from "../../model-view/model-view.component";
 import { TranslateDirective } from '@ngx-translate/core';
 import { NgIconComponent, provideIcons } from "@ng-icons/core";
@@ -11,7 +11,8 @@ import {
   remixTimeLine,
   remixHistoryLine,
   remixEditLine,
-  remixDeleteBin6Line
+  remixDeleteBin6Line,
+  remixBookMarkedLine
 } from '@ng-icons/remixicon';
 import { signals } from '../../../../common/signals';
 import { BaseChartDirective } from 'ng2-charts';
@@ -46,18 +47,35 @@ type DrainageRule = {
       remixHistoryLine,
       remixEditLine,
       remixDeleteBin6Line,
+      remixBookMarkedLine,
     }),
   ],
 })
 export class SimulationComponent {
+  constructor() {
+    effect(() => {
+      const data = this.rainForecast();
+      const chart = this.chart()?.chart;
+      
+      if (chart) {
+        chart.data.datasets[0].data = data;
+        chart.update();
+      }
+    });
+  }
+
+  protected chart = viewChild(BaseChartDirective);
   protected waterLevel: WritableSignal<number> = signal(50);
   protected checkedRainForecast: signals.ToggleableSignal = signals.toggleable(false);
   protected drainageRulesModalOpen: signals.ToggleableSignal = signals.toggleable(false);
   protected rainForecastModalOpen: signals.ToggleableSignal = signals.toggleable(false);
+  
+  protected rainForecastModal: WritableSignal<{x: string, y: number}[]> = signal([]);
+  protected rainForecast: WritableSignal<{x: string, y: number}[]> = signal([{x: '16:00', y: 0}, {x: '16:15', y: 0}, {x: '16:30', y: 3}, {x: '16:45', y: 2}, {x: '17:00', y: 0}, {x: '17:15', y: 0}, {x: '17:30', y: 6}, {x: '17:45', y: 8}]);
 
   dataRainForecast: ChartData<'bar', {x: string, y: number}[]> = {
     datasets: [{
-      data: [{x: '16:00', y: 0}, {x: '16:15', y: 0}, {x: '16:30', y: 3}, {x: '16:45', y: 2}, {x: '17:00', y: 0}, {x: '17:15', y: 0}, {x: '17:30', y: 6}, {x: '17:45', y: 8}],
+      data: this.rainForecast(),
       parsing: {
         xAxisKey: 'x',
         yAxisKey: 'y'
@@ -70,9 +88,62 @@ export class SimulationComponent {
     {title: "Starkregen", rainAmount: 15, rainDuration: 30, targetLevel: 20, drainageForerun: 30, open: signals.toggleable(false)},
   ];
   protected drainageRules: WritableSignal<DrainageRule[]>= signal(this.rules);
+  protected newDrainageRule: DrainageRule = {title: "", rainAmount: 0, rainDuration: 0, targetLevel: 0, drainageForerun: 0, open: signals.toggleable(true)}
+  protected drainageRuleModal: WritableSignal<DrainageRule>= signal(this.newDrainageRule);
+  protected drainageRuleModalIndex: WritableSignal<number|undefined> = signal(undefined);
   
   onToogleClick(event: MouseEvent, signal: signals.ToggleableSignal) {
     event.preventDefault();
     signal.toggle(); 
   };
+
+  copyDrainageRule(index: number) {
+    this.drainageRuleModal.set(this.drainageRules()[index]);
+  }
+
+  deleteDrainageRule(index: number) {
+    this.drainageRules.set(this.drainageRules().filter(( _ , i) => index !== i));
+  }
+
+  updateDrainageRules() {
+    if (this.drainageRuleModalIndex() === undefined) {
+      this.drainageRules().push(this.drainageRuleModal());
+    } else {
+      this.drainageRules.set(this.drainageRules().map((item, i) => 
+        i === this.drainageRuleModalIndex() ? this.drainageRuleModal() : item
+      ));
+    }
+  }
+
+  updateDrainageRuleTitle(value: string) {
+    this.drainageRuleModal.set({...this.drainageRuleModal(), title: value});
+  }
+
+  updateDrainageRule(attribute: string, value: number) {
+    switch (attribute) {
+      case "rainAmount":
+        this.drainageRuleModal.set({...this.drainageRuleModal(), rainAmount: value});
+        break;
+      case "rainDuration":
+        this.drainageRuleModal.set({...this.drainageRuleModal(), rainDuration: value});
+        break;
+      case "targetLevel":
+        this.drainageRuleModal.set({...this.drainageRuleModal(), targetLevel: value});
+        break;
+      case "drainageForerun":
+        this.drainageRuleModal.set({...this.drainageRuleModal(), drainageForerun: value});
+        break;
+    }
+  }
+
+  updateForecastModal(index: number, newY: number) {
+    const updated = this.rainForecastModal().map((item, i) =>
+      i === index ? { ...item, y: newY } : item
+    );
+    this.rainForecastModal.set(updated);
+  }
+
+  copyForecast(copy: WritableSignal<{x: string, y: number}[]>, copied: WritableSignal<{x: string, y: number}[]>) {
+    copy.set(copied().map(item => ({ ...item })));
+  }
 }
