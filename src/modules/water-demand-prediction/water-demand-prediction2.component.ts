40ad7d08a6fa4d52fc89dd77b3ffdebb6a51f29b
Tim Hesse <tim.hesse@uni-oldenburg.de>
2025-11-17 18:33:52 +0100

    linter fix

--- src/modules/water-demand-prediction/water-demand-prediction2.component.ts
+++ src/modules/water-demand-prediction/water-demand-prediction2.component.ts
@@ -2,8 +2,6 @@ import {effect, inject, Component} from "@angular/core";
 
 import {PmdArimaPredictionService} from "../../api/pmd-arima-prediction.service";
 
-type Service = PmdArimaPredictionService;
-
 @Component({
   imports: [],
   templateUrl: "./water-demand-prediction2.component.html",
