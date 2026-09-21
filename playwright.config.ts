dc2094e35dc9617ba7eef737ebec5ea9f41c368b
Tim Hesse <tim.hesse@uni-oldenburg.de>
2026-09-21 14:52:11 +0200

    make test screenshot and wait for map rendered

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,12 @@ export default defineConfig({
     trace: "on-first-retry",
   },
 
+  expect: {
+    toHaveScreenshot: {
+      pathTemplate: "{testDir}/{testFileDir}/e2e/screenshots/{projectName}/{arg}{ext}"
+    }
+  },
+
   /* Configure projects for major browsers */
   projects: [
     {
--- src/modules/growl/growl.e2e.ts
+++ src/modules/growl/growl.e2e.ts
@@ -1,6 +1,30 @@
-import {expect, test} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
-test("has title", async ({page}) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("@wisdom-oss/frontend");
+test("map renders as expected", async ({ page }) => {
+  let groundwaterMeasurementStations = page.waitForResponse((response) =>
+    response.url().endsWith(
+      "api/geodata/v2/content/groundwater_measurement_stations/",
+    ) && response.ok()
+  );
+
+    let groundwaterBodies = page.waitForResponse((response) =>
+    response.url().endsWith(
+      "api/geodata/v2/content/groundwater_bodies/",
+    ) && response.ok()
+  );
+
+  let groundwaterLevels = page.waitForResponse((response) =>
+    response.url().endsWith("api/groundwater-levels/graphql") && response.ok()
+  );
+
+  await page.goto("/growl");
+  await groundwaterMeasurementStations.then((response) => response.finished());
+  await groundwaterLevels.then((response) => response.finished());
+  await groundwaterBodies.then(response => response.finished());
+
+  await page.locator("main mgl-map").evaluate((mapElement) => new Promise<void>(resolve => {
+    (window as any).ng.getComponent(mapElement).mapInstance.once("idle", () => resolve())
+  }));
+
+  await expect(page.locator("main")).toHaveScreenshot();
 });
