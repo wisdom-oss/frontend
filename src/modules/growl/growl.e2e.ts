d569bba0545ec842d9ecbaa79004025eadc35fb6
Tim Hesse <tim.hesse@uni-oldenburg.de>
2026-09-21 14:59:14 +0200

    streamline test a bit

--- src/modules/growl/growl.e2e.ts
+++ src/modules/growl/growl.e2e.ts
@@ -1,30 +1,36 @@
 import { expect, test } from "@playwright/test";
 
 test("map renders as expected", async ({ page }) => {
-  let groundwaterMeasurementStations = page.waitForResponse((response) =>
-    response.url().endsWith(
-      "api/geodata/v2/content/groundwater_measurement_stations/",
-    ) && response.ok()
-  );
+  function waitForApiResponseOk(endpoint: string) {
+    return page.waitForResponse((response) =>
+      response.url().endsWith(endpoint) && response.ok()
+    );
+  }
 
-    let groundwaterBodies = page.waitForResponse((response) =>
-    response.url().endsWith(
-      "api/geodata/v2/content/groundwater_bodies/",
-    ) && response.ok()
+  const groundwaterMeasurementStations = waitForApiResponseOk(
+    "api/geodata/v2/content/groundwater_measurement_stations/",
   );
-
-  let groundwaterLevels = page.waitForResponse((response) =>
-    response.url().endsWith("api/groundwater-levels/graphql") && response.ok()
+  const groundwaterBodies = waitForApiResponseOk(
+    "api/geodata/v2/content/groundwater_bodies/",
+  );
+  const groundwaterLevels = waitForApiResponseOk(
+    "api/groundwater-levels/graphql",
   );
 
   await page.goto("/growl");
+  
   await groundwaterMeasurementStations.then((response) => response.finished());
   await groundwaterLevels.then((response) => response.finished());
-  await groundwaterBodies.then(response => response.finished());
+  await groundwaterBodies.then((response) => response.finished());
 
-  await page.locator("main mgl-map").evaluate((mapElement) => new Promise<void>(resolve => {
-    (window as any).ng.getComponent(mapElement).mapInstance.once("idle", () => resolve())
-  }));
+  await page.locator("main mgl-map").evaluate(
+    (mapElement) =>
+      new Promise<void>((resolve) => {
+        (window as any).ng
+          .getComponent(mapElement)
+          .mapInstance.once("idle", () => resolve());
+      }),
+  );
 
   await expect(page.locator("main")).toHaveScreenshot();
 });
