7ac79576ff01ba78dc020c5f90f98fa55af78cec
Tim Hesse <tim.hesse@uni-oldenburg.de>
2026-09-21 11:34:20 +0200

    reconfigure where tests should live

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,8 @@ import {defineConfig, devices} from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: "./src",
+  testMatch: "*.e2e.ts",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -20,7 +21,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env["CI"] ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -40,11 +41,6 @@ export default defineConfig({
     {
       name: "firefox",
       use: {...devices["Desktop Firefox"]},
-    },
-
-    {
-      name: "webkit",
-      use: {...devices["Desktop Safari"]},
     },
 
     /* Test against mobile viewports. */
--- src/modules/growl/growl.e2e.ts
+++ src/modules/growl/growl.e2e.ts
@@ -2,7 +2,5 @@ import {expect, test} from "@playwright/test";
 
 test("has title", async ({page}) => {
   await page.goto("/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/MyApp/);
+  await expect(page).toHaveTitle("@wisdom-oss/frontend");
 });
